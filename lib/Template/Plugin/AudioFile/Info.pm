a48e09c65fae46d8e4a66c76564b62aeb9fb5f61
Dave Cross <dave@perlhacks.com>
2008-04-20 17:01:37 +0000

    Import of DAVECROSS/Template-Plugin-AudioFile-Info-1.05 from CPAN.
    gitpan-cpan-distribution: Template-Plugin-AudioFile-Info
    gitpan-cpan-version:      1.05
    gitpan-cpan-path:         DAVECROSS/Template-Plugin-AudioFile-Info-1.05.tar.gz
    gitpan-cpan-author:       DAVECROSS
    gitpan-cpan-maturity:     released

--- lib/Template/Plugin/AudioFile/Info.pm
+++ lib/Template/Plugin/AudioFile/Info.pm
@@ -11,7 +11,7 @@ require Exporter;
 
 our @ISA = qw(Exporter AudioFile::Info Template::Plugin);
 
-our $VERSION = sprintf "%d.%02d", '$Revision: 1.4 $ ' =~ /(\d+)\.(\d+)/;
+our $VERSION = sprintf "%d", '$Revision: 17 $ ' =~ /(\d+)/;
 
 sub new {
   my ($class, $context, $file) = @_;
